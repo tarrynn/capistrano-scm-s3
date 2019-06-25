210ab783e3b753a1396d182dfd22fdd14b9eaf20
tarrynn <catalin.gheonea@gmail.com>
2019-06-25 14:01:22 +0100

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Capistrano::Scm::S3
+
+Grabs compressed source in tarball from S3
+
+```ruby
+group :development do
+  gem "capistrano", "~> 3.7", require: false
+  gem 'capistrano-scm-s3', '~> 0.1.6', git: 'https://github.com/tarrynn/capistrano-scm-s3.git'
+end
+```
+
+```
+bundle install
+```
+
+In `config/deploy.rb`:
+
+```ruby
+set :branch, 'staging'
+set :application, 'app-name'
+set :stage, 'stage'
+set :scm_s3_bucket, 's3://path/to/bucket'
+```
+
+Expects source tarball to be in `#{fetch :scm_s3_bucket}/#{fetch :stage}/#{fetch :branch}_source.tar.gz`
+
+It downloads the tarball into `/tmp/bootup`, extracts it there, moves contents into `release_path` then removes `/tmp/bootup`
--- lib/capistrano/scm/s3.rb
+++ lib/capistrano/scm/s3.rb
@@ -15,9 +15,9 @@ module Capistrano
                 on release_roles :all do
                   execute :mkdir, "-p", release_path
                   execute "mkdir /tmp/bootup"
-                  execute "cd /tmp/bootup && aws s3 cp s3://bergamotte-deployments/codebuild/#{fetch :stage}/#{fetch :branch}_source.tar.gz #{fetch :branch}_source.tar.gz"
+                  execute "cd /tmp/bootup && aws s3 cp #{fetch :scm_s3_bucket}/#{fetch :stage}/#{fetch :branch}_source.tar.gz #{fetch :branch}_source.tar.gz"
                   execute "cd /tmp/bootup && tar xf #{fetch :branch}_source.tar.gz"
-                  execute "mv /tmp/bootup/bergamotte/* #{release_path}"
+                  execute "mv /tmp/bootup/#{fetch :application}/* #{release_path}"
                   execute "rm -rf /tmp/bootup"
                 end
               end
