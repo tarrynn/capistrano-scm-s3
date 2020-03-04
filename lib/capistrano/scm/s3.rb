56491ef947d3496ac20f3a7d80069377bfcde7b3
tarrynn <catalin.gheonea@gmail.com>
2020-03-04 12:08:17 +0000

    trying on role

--- lib/capistrano/scm/s3.rb
+++ lib/capistrano/scm/s3.rb
@@ -1,8 +1,5 @@
 require "capistrano/scm/s3/version"
 require "capistrano/scm/plugin"
-require 'sshkit'
-require 'sshkit/dsl'
-include SSHKit::DSL
 
 module Capistrano
   module Scm
@@ -29,7 +26,10 @@ module Capistrano
               end
 
               task :set_current_revision do
-                set(:current_revision, capture("cat #{release_path}/.git/HEAD"))
+                on release_roles :all do
+                  revision = capture(:cat, "#{release_path}/.git/HEAD")
+                  set(:current_revision, revision)
+                end
               end
             end
           end
--- lib/capistrano/scm/s3/version.rb
+++ lib/capistrano/scm/s3/version.rb
@@ -1,7 +1,7 @@
 module Capistrano
   module Scm
     module S3
-      VERSION = "0.1.14"
+      VERSION = "0.1.15"
     end
   end
 end
